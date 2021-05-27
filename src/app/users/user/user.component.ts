94fa4b5af25b9cbe1d617a4dfc8ea9b75d0f0edf
Waqas <wakassadik@gmail.com>
2021-05-27 10:16:07 +0500

    getting route params Reactively

--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-user',
@@ -16,6 +16,16 @@ export class UserComponent implements OnInit {
       id: this.route.snapshot.params['id'],
       name: this.route.snapshot.params['name']
     };
+    // its observeable are features by 3rd patry
+    // allows you to work wit async tasks.
+    // this would only run when param is changed
+    this.route.params
+      .subscribe(
+        (params: Params) => {
+          this.user.id = params['id'],
+          this.user.name = params['name']
+        }
+      );
   }
 
 }
