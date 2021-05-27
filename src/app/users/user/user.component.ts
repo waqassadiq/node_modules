3e993d5edcba4912ddc76af876670c9b58cbaa1c
Waqas <wakassadik@gmail.com>
2021-05-27 10:28:07 +0500

    using ngDestory to unsubscribe the subscription to destory the subscruption using lifecycle hooks

--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -1,14 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
-export class UserComponent implements OnInit {
+export class UserComponent implements OnInit, OnDestroy {
   user: {id: number, name: string};
-
+  parameterSubscription: Subscription;
   constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
@@ -19,13 +20,21 @@ export class UserComponent implements OnInit {
     // its observeable are features by 3rd patry
     // allows you to work wit async tasks.
     // this would only run when param is changed
-    this.route.params
+    // Note: eveen though you page is destoryed but the subsction
+    // stauys in the mentory so use OnDestory, to destory it
+    this.parameterSubscription = this.route.params
       .subscribe(
         (params: Params) => {
           this.user.id = params['id'],
           this.user.name = params['name']
         }
       );
+  }
+
+  ngOnDestroy(){
+    // now this would be destoryed with the component
+    // it's optional, now here, but it's good to write it down
+    this.parameterSubscription.unsubscribe();
   }
 
 }
