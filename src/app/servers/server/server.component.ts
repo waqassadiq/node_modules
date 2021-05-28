2bb83e02bc093d6c2e69c65b0a3c5c447c2fd332
Waqas <wakassadik@gmail.com>
2021-05-28 22:39:25 +0500

    setting up child routes for server

--- src/app/servers/server/server.component.ts
+++ src/app/servers/server/server.component.ts
@@ -25,7 +25,7 @@ export class ServerComponent implements OnInit {
     this.server = this.serversService.getServer(id);
     this.route.params.subscribe(
       (params: Params) => {
-        this.server = this.serversService.getServer(params['id']);
+        this.server = this.serversService.getServer(+params['id']);
       }
     );
   }
