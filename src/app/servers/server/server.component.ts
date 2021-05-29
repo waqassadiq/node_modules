88998ffe5089f61d590fd6a44c999364cdf4d895
Waqas <wakassadik@gmail.com>
2021-05-29 14:21:36 +0500

    preserving the queryparam to edit page

--- src/app/servers/server/server.component.ts
+++ src/app/servers/server/server.component.ts
@@ -36,7 +36,7 @@ export class ServerComponent implements OnInit {
   }
 
   onEdit(){
-    this.router.navigate(['edit'], {relativeTo: this.route});
+    this.router.navigate(['edit'], {relativeTo: this.route, queryParamsHandling: 'preserve'});
   }
 
 }
